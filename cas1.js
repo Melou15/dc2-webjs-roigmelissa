523be2a5146e7a333d3a1126c49e02d5535e0a68
Melissa Roig <melissa.roig@laposte.net>
2023-02-15 11:09:12 +0100

    création de 3 div dans l'id + un enfant et remplacement de l'enfant

--- cas1.js
+++ cas1.js
@@ -4,3 +4,9 @@ elt.innerHTML = "<div>texte 1</div><div>texte2</div><div>texte 3</div>";
 let newElm = document.createElement("div");
 newElm.innerHTML = "<div>texte4</div>";
 elt.appendChild(newElm);
+
+//* Remplacer élément que l'on à par une section et 2 span"//
+
+elt.replaceChild(document.createElement("section"), newElm);
+newElm.innerHTML = "<section><p><span>coucou je test quelque chose</span></p><p><span>coucou je test autre chose </span></p></section>";
+elt.appendChild(newElm);
