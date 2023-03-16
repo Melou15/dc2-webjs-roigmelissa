791a63d119f2c33e5b7561b8eb6010850326e1b2
Melissa Roig <melissa.roig@laposte.net>
2023-03-16 08:36:38 +0100

    correction du code pour url

--- cas1.js
+++ cas1.js
@@ -65,11 +65,36 @@ var search_params = new URLSearchParams(url.search);
 if (search_params.has('name')){
 	var name = search_params.get('name');
 	console.log(name);
+}
+if (search_params.has('id')){	
 	var id = search_params.get('id');
 	console.log(id);
+}	
+if (search_params.has('lastname')){	
 	var lastname = search_params.get('lastname');
 	console.log(lastname);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
