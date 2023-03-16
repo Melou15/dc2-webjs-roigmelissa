35530b36e6c7a6599fa051c8a29cbc89d3fab295
Melissa Roig <melissa.roig@laposte.net>
2023-03-16 08:27:10 +0100

    exercice url

--- cas1.js
+++ cas1.js
@@ -58,7 +58,18 @@ ret.addEventListener('click',function(){
 	
 
 
+var str = "https://www.domain.com/page?name=francois1&id=opwiueru3920983r2&lastname=john";
+var url = new URL(str);
+var search_params = new URLSearchParams(url.search);
 
+if (search_params.has('name')){
+	var name = search_params.get('name');
+	console.log(name);
+	var id = search_params.get('id');
+	console.log(id);
+	var lastname = search_params.get('lastname');
+	console.log(lastname);
+}
 
 
 
